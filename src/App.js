e9a30f62bd30ec6c97e3239769a2aea1cb9f3ae3
mided3v <ayo4real6@gmail.com>
2023-08-13 20:16:45 +0100

    page structure update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter,  Routes, Route } from 'react-router-dom'
+
+// import Button from "@material-ui/core/Button";
+
+// COMPONENTS
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Footer from "./components/footer/Footer";
+import Movies from "./components/movies/Movies";
+import Series from "./components/series/Series";
+import Searchbar from "./components/searchbar/Searchbar";
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Searchbar/>
+      {/* <Button variant='contained' size='large'>Hello</Button> */}
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/movies" element={<Movies/>}/>
+        <Route path="/series" element={<Series/>}/>
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
